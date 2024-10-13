04997ab8559ade6c8be35459d2e9f991d2955afc
Haztec <cjmassey47@gmail.com>
2024-10-13 20:41:10 +0100

    Got training and inference working

--- trainandgetdata.py
+++ trainandgetdata.py
@@ -108,9 +108,10 @@ def causal_mask(size):
 
 # Configuration code
 def get_config():
+    
     return {
         "batch_size": 8,
-        "num_epochs": 1,
+        "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
